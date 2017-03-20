52127696db463c48f917c79bf0a2720ec8e57e55
myamamoto88 <myamamoto@aiming-inc.com>
2017-03-21 01:06:36 +0900

    Update usage

--- lgtm.go
+++ lgtm.go
@@ -8,7 +8,7 @@ import (
 func main() {
   app := cli.NewApp()
   app.Name = "lgtm"
-  app.Usage = "Make the LGTM image"
+  app.Usage = "Make the LGTM image, and copy clipboard"
   app.Action = doAction
   app.Run(os.Args)
 }
