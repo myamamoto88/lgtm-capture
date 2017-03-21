596a9d9a08ef673fd3bea3aea24ee78483413eba
myamamoto88 <myamamoto@aiming-inc.com>
2017-03-22 00:10:42 +0900

    Dump version 1.0.0

--- lgtm-capture.go
+++ lgtm-capture.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
   app := cli.NewApp()
+  app.Version = "1.0.0"
   app.Name = "lgtm-capture"
   app.Usage = "Capture screen, then compose image for LGTM, then copy it to the clipboard."
   app.Action = doAction
